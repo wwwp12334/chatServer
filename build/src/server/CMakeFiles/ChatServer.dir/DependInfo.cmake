078304eeb709a558fd3ff7c384a81448e023844e
wwwp12334 <2232852166@qq.com>
2024-03-03 18:09:16 +0800

    first commit

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/test/chatserver-master/src/server/chatserver.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/test/chatserver-master/src/server/chatservice.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/test/chatserver-master/src/server/db/db.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/test/chatserver-master/src/server/main.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/test/chatserver-master/src/server/model/friendmoel.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/model/friendmoel.cpp.o"
+  "/test/chatserver-master/src/server/model/groupmodel.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/test/chatserver-master/src/server/model/offlinemessagemodel.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/test/chatserver-master/src/server/model/usermodel.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/test/chatserver-master/src/server/redis/redis.cpp" "/test/chatserver-master/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
